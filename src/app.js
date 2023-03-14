2f51c4eff6d8680ab7a66324d7eb422a18fbe319
DRGWeiss <ruizd1710@gmail.com>
2023-03-14 11:10:49 -0400

    event listeners for all buttons

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ let displayValue = 0;
 
 function changeDisplayValue(newValue) {
   displayValue = newValue;
+  console.log(displayValue)
   // code for updating the screen with new value
 }
 
@@ -18,14 +19,38 @@ function onClear() {}
 
 function undo() {}
 
+function onEqual() {
+  // operate function imported from operations
+}
+
 function handleEventListeners() {
-  let numberButtons = document.querySelectorAll('.number');
+  const numberButtons = document.querySelectorAll('.number');
   numberButtons.forEach(element => {
-    element.addEventListener("click", (e) => {
+    element.addEventListener("click", () => {
       // The text written on the button, which will be the number itself
       onNumberSelect(element.childNodes[0].nodeValue);
     });
   });
+  const operatorButtons = document.querySelectorAll('.operator');
+  operatorButtons.forEach(element => {
+    element.addEventListener("click", () => {
+      // The text written on the button, which will be the operator itself
+      // A string contained +, -, * or /
+      onOperatorSelect(element.childNodes[0].nodeValue);
+    });
+  });
+  const equalButton = document.querySelector('.equal');
+  equalButton.addEventListener("click", () => {
+    onEqual();
+  });
+  const clearButton = document.querySelector('.clear')
+  clearButton.addEventListener("click", () => {
+    onClear();
+  });
+  const undoButton = document.querySelector('.undo')
+  undoButton?.addEventListener("click", () => {
+    undo();
+  })
 }
 
 handleEventListeners();
