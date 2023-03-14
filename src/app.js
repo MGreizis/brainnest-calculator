771f2ba8e974cc6d0e156b4b2b5ce6b77e3f93b4
DRGWeiss <ruizd1710@gmail.com>
2023-03-14 10:30:55 -0400

    event listener for number buttons

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
-import { add, subtract, multiply, divide, operate } from "./operations";
+// import * as operations from "./operations";
+// This import here won't work if running the site by opening the file directly, it needs a live server
 
 let displayValue = 0;
 
-function changeDisplayValue(newValue) {}
+function changeDisplayValue(newValue) {
+  displayValue = newValue;
+  // code for updating the screen with new value
+}
 
-function onNumberSelect(number) {}
+function onNumberSelect(number) {
+  changeDisplayValue(number);
+}
 
-function handleEventListeners() {}
+function handleEventListeners() {
+  let numberButtons = document.querySelectorAll('.number');
+  numberButtons.forEach(element => {
+    element.addEventListener("click", (e) => {
+      // The text written on the button, which will be the number itself
+      onNumberSelect(element.childNodes[0].nodeValue);
+    });
+  });
+}
+
+handleEventListeners();
