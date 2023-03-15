68175678de8f7d74c90e3772a315f232cdb7419e
Jasline <jassy11992@gmail.com>
2023-03-14 20:01:51 -0400

    Added functions for equations

--- src/operations.js
+++ src/operations.js
@@ -1,8 +1,62 @@
-function add(num1, num2) {}
-function subtract(num1, num2) {}
-function multiply(num1, num2) {}
-function divide(num1, num2) {}
+function add(number, newValue) {
+    if (operator === '+') {
+        return number + newValue;
+    }
+}
 
-function operate(operator, num1, num2) {}
+function subtract(number, newValue) {
+    if (operator === '-') {
+        return number - newValue;
+    }
+}
+
+
+function multiply(number, newValue) {
+    if (operator === '*') {
+        return number * newValue; 
+    }
+}
+
+function divide(number, newValue) {
+    if (operator === '/') {
+        return number / newValue;
+    }
+}
+
+function operate(number, newValue, operator) {
+    const newValue = parseFloat(displayValue);
+    const result = calculate(number, newValue, operator)
+    displayValue = String(result);
+}
 
 export {add, subtract, multiply, divide, operate}
+
+
+/* potentially what we really need
+
+let divide = operators.indexOf("÷");
+  while (divide != -1) {
+    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+    operators.splice(divide, 1);
+    divide = operators.indexOf("÷");
+  }
+  
+*/
+
+
+
+//Can I do this? //
+function calculate(number, newValue, operator) {
+    if (operator === '+') {
+        return number + newValue;
+    } else if (operator === '-') {
+        return number - newValue;
+    } else if (operator === '*') {
+        return number * newValue; 
+    } else if (operator === '/') {
+        return number / newValue;
+    }
+    return newValue
+} export {calculate};
+
+//result displayed ===true;//
