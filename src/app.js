5089d620d94fce3d0d0e40c55b39405f22ab2e2e
Martins Greizis <grei0003@hz.nl>
2023-03-17 16:49:29 +0100

    delete button now removes last char entered

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 let firstOperand = "";
 let secondOperand = "";
-let operator = ""
-let displayValue = ''
+let operator = "";
+let displayValue = "";
 
 function add(num1, num2) {
   return num1 + num2;
@@ -10,19 +10,19 @@ function subtract(num1, num2) {
   return num1 - num2;
 }
 function multiply(num1, num2) {
-  return num1 * num2; 
+  return num1 * num2;
 }
 function divide(num1, num2) {
   return num1 / num2;
 }
 function operate(num1, num2, operator) {
-  if (operator === '+') {
+  if (operator === "+") {
     return num1 + num2;
-  } else if (operator === '-') {
+  } else if (operator === "-") {
     return num1 - num2;
-  } else if (operator === '*') {
-    return num1 * num2; 
-  } else if (operator === '/') {
+  } else if (operator === "*") {
+    return num1 * num2;
+  } else if (operator === "/") {
     return num1 / num2;
   }
 }
@@ -33,7 +33,7 @@ function changeDisplayValue(newValue, replace = false) {
   // } else {
   //   displayValue += newValue;
   // }
-  replace ? displayValue = newValue : displayValue += newValue;
+  replace ? (displayValue = newValue) : (displayValue += newValue);
   let displayNode = document.querySelector(".screen");
   displayNode.textContent = displayValue;
 }
@@ -70,7 +70,6 @@ function onOperatorSelect(newOperator) {
       changeDisplayValue(operator);
     }
   }
-  
 }
 
 function onClear() {
@@ -83,14 +82,28 @@ function onClear() {
 
 function onDelete() {
   if (displayValue.length > 0) {
-    // turn string into array
-    const arrayDisplayValue = displayValue.split("")
-    // remove the last element and turn it into a string again
-    displayValue = arrayDisplayValue.slice(0, arrayDisplayValue.length - 1).join("");
-    // force displayValue on screen to be replaced
-    changeDisplayValue(displayValue, true);
+    // check if displayValue is a result of a previous calculation
+    if (/\d+.?\d*\s*[+-/]\s\d+.?\d*/.test(displayValue)) {
+      let result = displayValue.split(" ")[2];
+      displayValue = displayValue.slice(0, -result.length);
+      firstOperand = result;
+      secondOperand = "";
+      operator = "";
+    } else {
+      // remove last character from displayValue
+      displayValue = displayValue.slice(0, -1);
+      if (operator) {
+        // if deleting from second operand
+        secondOperand = secondOperand.slice(0, -1);
+      } else {
+        // if deleting from first operand
+        firstOperand = firstOperand.slice(0, -1);
+      }
+    }
+    // update display
+    let displayNode = document.querySelector(".screen");
+    displayNode.textContent = displayValue;
   }
-  
 }
 
 /* does something like this work??
@@ -111,49 +124,49 @@ function onEqual() {
 */
 
 function onEqual() {
-  let result
   if (firstOperand && secondOperand && operator) {
-    result = operate(Number(firstOperand), Number(secondOperand), operator);
-    // take the result as the new first operand for following operations
-    // and clear the second operand
-    firstOperand = String(result);
+    let result = operate(
+      parseFloat(firstOperand),
+      parseFloat(secondOperand),
+      operator
+    );
+    // reset operands and operator
+    firstOperand = result.toString();
     secondOperand = "";
-  } else {
-    return;
+    operator = "";
+    // update display with result
+    changeDisplayValue(result.toString(), true);
   }
-  // switch display value to only the result
-  changeDisplayValue(result, true)
 }
 
-
 function handleEventListeners() {
-  const numberButtons = document.querySelectorAll('.number');
-  numberButtons.forEach(element => {
+  const numberButtons = document.querySelectorAll(".number");
+  numberButtons.forEach((element) => {
     element.addEventListener("click", () => {
       // The text written on the button, which will be the number itself
       onNumberSelect(element.textContent);
     });
   });
-  const operatorButtons = document.querySelectorAll('.operator');
-  operatorButtons.forEach(element => {
+  const operatorButtons = document.querySelectorAll(".operator");
+  operatorButtons.forEach((element) => {
     element.addEventListener("click", () => {
       // The text written on the button, which will be the operator itself
       // A string contained +, -, * or /
       onOperatorSelect(element.textContent);
     });
   });
-  const equalButton = document.querySelector('.equal');
+  const equalButton = document.querySelector(".equal");
   equalButton.addEventListener("click", () => {
     onEqual();
   });
-  const clearButton = document.querySelector('.clear')
+  const clearButton = document.querySelector(".clear");
   clearButton.addEventListener("click", () => {
     onClear();
   });
-  const deleteButton = document.querySelector('#delete')
+  const deleteButton = document.querySelector("#delete");
   deleteButton?.addEventListener("click", () => {
     onDelete();
-  })
+  });
 }
 
 handleEventListeners();
